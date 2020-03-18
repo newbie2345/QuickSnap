fd3c369eb0cf3ee28e89c19255f42c56b7e05325
Tiong Wei Yuan <101214349@students.swinburne.edu.my>
2020-03-19 01:17:57 +0800

    Drawn using a more interesting font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -63,15 +63,16 @@ namespace CardGames
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
 		private static void DrawGame(Snap myGame)
 		{
+			SwinGame.LoadFontNamed("GameFont","Chunkfive.otf",24);
 			SwinGame.DrawBitmap("cardsBoard.png",0,0);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White,"GameFont", 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521,153);
 			}
 			else
